ffa86b02ff79645505f26b6641c7e2ebe8c37896
janujk04 <janu_jk@outlook.com>
2026-04-08 22:31:20 +0530

    First commit

---
+++ firstprogram.py
@@ -0,0 +1,21 @@
+a= int(input("enter the value of a= ")) 
+b= int(input("enter the value of b= "))
+sum = int(a) + int(b)
+print("the value of a",a)
+print("the sum is ",sum)
+difference = int(a) - int(b)
+print("the difference is ",difference)
+multiply = int(a) * int(b)
+print(" the product is ", multiply)
+if b % 2 == 0:
+    print(b, "is even number")
+else:
+    print(b, "it is odd number")
+if a > b:
+    largest = a
+else:
+    largest = b
+print("the largest number is", largest)
+switcher = {
+
+	
