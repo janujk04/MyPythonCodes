bca4f9ecc58724247042784089e0098505d9ee60
janujk04 <janu_jk@outlook.com>
2026-04-09 19:29:23 +0530

    latest changes

--- firstprogram.py
+++ firstprogram.py
@@ -1,9 +1,15 @@
 a= int(input("enter the value of a= ")) 
 b= int(input("enter the value of b= "))
 sum = int(a) + int(b)
+
 print("the value of a",a)
+
+#The below line will print sum of two numbers
 print("the sum is ",sum)
+
+#The below line is for difference
 difference = int(a) - int(b)
+
 print("the difference is ",difference)
 multiply = int(a) * int(b)
 print(" the product is ", multiply)
